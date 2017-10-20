95f665525ebf7bb57dce6ef467a0fad0ba4b9a70
AldiT <aldi_topalli@yahoo.com>
2017-10-21 00:10:45 +0200

    Initial Commit

---
+++ Perceptron/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Perceptron
+//
+//  Created by Aldi Topalli on 21/10/2017.
+//  Copyright © 2017 CodeArt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
